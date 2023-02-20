5bd949b319f095bf594f8e767a2ffb65d35732a0
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-02-20 12:25:57 +0530

    Update voltage_beryllium.mk

--- voltage_beryllium.mk
+++ voltage_beryllium.mk
@@ -6,19 +6,36 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-
-# Unofficial-ify
-VOLTAGE_BUILD_TYPE := UNOFFICIAL
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Bootanimation Resolution
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := false
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_ALERT_SLIDER := false
+
+# Maintainer Flag
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.aosp.maintainer=PSKMaxi
+
+# Unofficial
+AOSP_MAINTAINER := PSKMaxi
+AOSP_BUILD_TYPE := UNOFFICIAL
+BUILD_USERNAME := PSKMaxi
+BUILD_HOSTNAME := Aosp
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := voltage_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
