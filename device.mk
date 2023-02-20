03e59559ff0523bfd782fb28ca5f0eeacc039198
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-02-20 12:27:23 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pixelexperience
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
