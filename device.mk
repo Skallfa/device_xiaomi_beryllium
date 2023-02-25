ec39e06644d14a3d928641b42d46f879b131a52f
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-02-25 21:42:00 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-    BerylliumNoCutoutOverlay
+    NoCutoutOverlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
