7a0412fd5798dbaf96f88693666fe0dd65fa150b
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-03-14 12:43:23 +0530

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 NfcNci MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM Turbo TurboPrebuilt TurboAdapter Videos Camera2 SafetyHubPrebuilt arcore CarrierSetup Chrome-Stub ConnMetrics DMService NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions SCONE VZWAPNLib VzwOmaTrigger obdm_stub GooglePartnerSetupPrebuilt SoterServicePrebuilt
+LOCAL_OVERRIDES_PACKAGES := GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 NfcNci MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM Turbo TurboPrebuilt TurboAdapter Videos Camera2 SafetyHubPrebuilt arcore CarrierSetup Chrome-Stub ConnMetrics DMService NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions SCONE VZWAPNLib VzwOmaTrigger obdm_stub GooglePartnerSetup SoterService
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
