8e52e1df5c715534bd947181499f16acc67ee4cf
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-01 15:22:49 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Skallfa/device_xiaomi_beryllium_sdm845-common.git -
 rm -rf prebuilts/clang/host/linux-x86/trb_clang && git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/trb_clang
 
 # Clone Hals
-rm -rf hardware/qcom-caf/sdm845/display && git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.1-caf-sdm845 hardware/qcom-caf/sdm845/display
-rm -rf hardware/qcom-caf/sdm845/audio && git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-12.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
-rm -rf hardware/qcom-caf/sdm845/media && git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-12.1-caf-sdm845 hardware/qcom-caf/sdm845/media
-rm -rf external/tinyxml && git clone https://github.com/ArrowOS/android_external_tinyxml -b arrow-13.0 external/tinyxml
+rm -rf hardware/qcom-caf/sdm845/display && git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/display
+rm -rf hardware/qcom-caf/sdm845/audio && git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
+rm -rf hardware/qcom-caf/sdm845/media && git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/media
+rm -rf external/tinyxml && git clone https://github.com/ArrowOS/android_external_tinyxml -b arrow-13.1 external/tinyxml
