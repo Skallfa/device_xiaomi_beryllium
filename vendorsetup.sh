1870b7429ffbc6e58613aab4915a41aef9171fb6
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-06-20 15:07:19 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Clone Kernel
-rm -rf kernel/xiaomi && git clone https://github.com/Krtonia/kawaii_kernel_sdm845.git -b 13 kernel/xiaomi/sdm845 --depth=1
+rm -rf kernel/xiaomi && git clone https://github.com/PSKMaxi/kernel_xiaomi_pocophone-f1.git -b 13 kernel/xiaomi/sdm845 --depth=1
 
 # Clone Vendor
 rm -rf vendor/xiaomi && git clone https://github.com/PSKMaxi/xiaomi_beryllium_vendor.git -b main vendor/xiaomi --depth=1
@@ -9,7 +9,7 @@ git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b pex devi
 
 
 # Clone Clang
-rm -rf prebuilts/clang/host/linux-x86/trb_clang && git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/trb_clang
+# rm -rf prebuilts/clang/host/linux-x86/trb_clang && git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/trb_clang
 
 # Clone Hals
 rm -rf hardware/qcom-caf/sdm845/display && git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/display
