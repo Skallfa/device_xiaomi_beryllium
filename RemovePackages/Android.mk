eb16f15688404500ffec6b33ab3d641e82d4148d
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-15 11:49:33 +0530

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail RecorderPrebuilt Maps Chrome GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 NfcNci MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM Turbo TurboPrebuilt TurboAdapter Videos Camera2 SafetyHubPrebuilt arcore CarrierSetup Chrome-Stub ConnMetrics DMService NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions SCONE VZWAPNLib VzwOmaTrigger obdm_stub SoterService Aperture Photos
+LOCAL_OVERRIDES_PACKAGES := PrebuiltGmail RecorderPrebuilt Maps Chrome GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM Turbo TurboPrebuilt TurboAdapter Videos Camera2 SafetyHubPrebuilt arcore CarrierSetup Chrome-Stub ConnMetrics DMService NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions SCONE VZWAPNLib VzwOmaTrigger obdm_stub SoterService Aperture Photos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
