ee48a32c621aa494eafc7a76a7b6a40346ed0c40
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-16 18:51:27 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf kernel/xiaomi && git clone https://github.com/Skallfa/kernel_xiaomi_sdm84
 rm -rf vendor/xiaomi && git clone https://github.com/PSKMaxi/xiaomi_beryllium_vendor.git -b main vendor/xiaomi --depth=1
 
 # Clone Device Trees
-git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b pex device/xiaomi/sdm845-common
+git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b peplus device/xiaomi/sdm845-common
 
 # clone miui cam
 git clone https://github.com/resist15/vendor_miuicamera.git -b arrow-13.1 vendor/miuicamera
