47339cd5d6302b54709217e5a66d2c3cfc7a17b0
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-29 21:14:12 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
+# clone custom additions
+rm -rf frameworks/base && git clone https://github.com/PSKMaxi/fwb.git -b thirteen-plus frameworks/base --depth=1
+rm -rf packages/apps/Settings && git clone https://github.com/PSKMaxi/pas.git -b thirteen-plus packages/apps/Settings --depth=1
+rm -rf frameworks/av && git clone https://github.com/PSKMaxi/fwa.git -b thirteen frameworks/av --depth=1
+rm -rf vendor/aosp && git clone https://github.com/PSKMaxi/va.git -b thirteen-plus vendor/aosp --depth=1
+rm -rf vendor/pixel-framework && git clone https://github.com/PSKMaxi/vpf.git -b thirteen-plus vendor/pixel-framework --depth=1
+
 # Clone Kernel
 rm -rf kernel/xiaomi && git clone https://github.com/Skallfa/kernel_xiaomi_sdm845-common.git -b 13 kernel/xiaomi/sdm845-common --depth=1
 
@@ -8,7 +15,7 @@ rm -rf vendor/xiaomi && git clone https://github.com/PSKMaxi/xiaomi_beryllium_ve
 git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b peplus device/xiaomi/sdm845-common
 
 # clone miui cam
-git clone https://github.com/resist15/vendor_miuicamera.git -b arrow-13.1 vendor/miuicamera
+git clone https://github.com/resist15/vendor_miuicamera.git -b arrow-13.1 vendor/miuicamera --depth=1
 
 # Clone Clang
 git clone https://gitlab.com/jjpprrrr/prelude-clang.git -b master clang-prelude --depth=1 && mv clang-prelude prebuilts/clang/host/linux-x86/
