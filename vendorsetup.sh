db1f877872f9ee1c250cb6ae06f369dda3b9fcd9
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-29 10:10:29 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Clone Kernel
-rm -rf kernel/xiaomi && git clone https://github.com/PSKMaxi/kernel_xiaomi_sdm845-common.git -b dev-cputuning kernel/xiaomi/sdm845-common --depth=1
+rm -rf kernel/xiaomi && git clone https://github.com/Skallfa/kernel_xiaomi_sdm845-common.git -b 13 kernel/xiaomi/sdm845-common --depth=1
 
 # Clone Vendor
 rm -rf vendor/xiaomi && git clone https://github.com/PSKMaxi/xiaomi_beryllium_vendor.git -b main vendor/xiaomi --depth=1
@@ -11,7 +11,7 @@ git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b peplus d
 git clone https://github.com/resist15/vendor_miuicamera.git -b arrow-13.1 vendor/miuicamera
 
 # Clone Clang
-# rm -rf prebuilts/clang/host/linux-x86/clang-proton && git clone https://github.com/kdrag0n/proton-clang.git -b master --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+git clone https://gitlab.com/jjpprrrr/prelude-clang.git -b master clang-prelude --depth=1 && mv clang-prelude prebuilts/clang/host/linux-x86/
 
 # Clone Hals
 rm -rf hardware/qcom-caf/sdm845/display && git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/display
