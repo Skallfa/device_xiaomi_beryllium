4b8562c07c953a595bd54902f2b125dfb4632772
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-01-09 17:47:01 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,7 @@ git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b peplus d
 # git clone https://github.com/resist15/vendor_miuicamera.git -b arrow-13.1 vendor/miuicamera --depth=1
 
 # Clone Clang
-# git clone https://gitlab.com/jjpprrrr/prelude-clang.git -b master clang-prelude --depth=1 && mv clang-prelude prebuilts/clang/host/linux-x86/
+git clone https://gitlab.com/jjpprrrr/prelude-clang.git -b master clang-prelude --depth=1 && mv clang-prelude prebuilts/clang/host/linux-x86/
 
 # Clone Hals
 rm -rf hardware/qcom-caf/sdm845/display && git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.1-caf-sdm845 hardware/qcom-caf/sdm845/display
