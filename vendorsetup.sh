4b76bfbb92baa2751b39143dbe6b9a9f602b2ae8
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-01-12 13:23:45 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
 # clone custom additions
-rm -rf frameworks/base && git clone https://github.com/PSKMaxi/fwb.git -b thirteen-plus frameworks/base --depth=1
-rm -rf packages/apps/Settings && git clone https://github.com/PSKMaxi/pas.git -b thirteen-plus packages/apps/Settings --depth=1
+rm -rf frameworks/base && git clone https://github.com/Skallfa/fwb.git -b thirteen-plus frameworks/base --depth=1
+rm -rf packages/apps/Settings && git clone https://github.com/Skallfa/pas.git -b thirteen-plus packages/apps/Settings --depth=1
 rm -rf frameworks/av && git clone https://github.com/PSKMaxi/fwa.git -b thirteen frameworks/av --depth=1
-rm -rf vendor/aosp && git clone https://github.com/PSKMaxi/va.git -b thirteen-plus vendor/aosp --depth=1
+rm -rf vendor/aosp && git clone https://github.com/Skallfa/va.git -b thirteen-plus vendor/aosp --depth=1
 rm -rf vendor/pixel-framework && git clone https://github.com/PSKMaxi/vpf.git -b thirteen-plus vendor/pixel-framework --depth=1
+rm -rf device/custom/sepolicy && git clone https://github.com/Skallfa/device_custom_sepolicy.git -b thirteen device/custom/sepolicy
+rm -rf vendor/custom-preference && git clone https://github.com/Skallfa/vendor_custom-preference.git -b thirteen vendor/custom-preference
 
 # Clone Kernel
 rm -rf kernel/xiaomi && git clone https://github.com/Krtonia/Radioactive_kernel.git -b 14 kernel/xiaomi/sdm845-common --depth=1
@@ -27,4 +29,4 @@ rm -rf hardware/qcom-caf/sdm845/media && git clone https://github.com/ArrowOS/an
 rm -rf external/tinyxml && git clone https://github.com/ArrowOS/android_external_tinyxml -b arrow-13.1 external/tinyxml
 
 # Clone Kernel SU
-cd kernel/xiaomi/sdm845-common/ && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
+# cd kernel/xiaomi/sdm845-common/ && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
