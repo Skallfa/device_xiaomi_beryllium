e63a3aff6230ee85dadcfd2ea2ef4c14652f6a80
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-02-17 20:00:28 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,13 @@
 # clone custom additions
-rm -rf frameworks/base && git clone https://github.com/Skallfa/fwb.git -b wip frameworks/base --depth=1
-rm -rf packages/apps/Settings && git clone https://github.com/Skallfa/pas.git -b wip packages/apps/Settings --depth=1
+rm -rf frameworks/base && git clone https://github.com/Skallfa/fwb.git -b thirteen-plus frameworks/base --depth=1
+rm -rf packages/apps/Settings && git clone https://github.com/Skallfa/pas.git -b thirteen-plus packages/apps/Settings --depth=1
 rm -rf frameworks/av && git clone https://github.com/PSKMaxi/fwa.git -b thirteen frameworks/av --depth=1
 rm -rf vendor/aosp && git clone https://github.com/Skallfa/va.git -b thirteen-plus vendor/aosp --depth=1
 rm -rf vendor/pixel-framework && git clone https://github.com/PSKMaxi/vpf.git -b thirteen-plus vendor/pixel-framework --depth=1
 rm -rf device/custom/sepolicy && git clone https://github.com/Skallfa/device_custom_sepolicy.git -b thirteen device/custom/sepolicy
 rm -rf vendor/custom-preference && git clone https://github.com/Skallfa/vendor_custom-preference.git -b thirteen vendor/custom-preference
 rm -rf build/soong && git clone https://github.com/Skallfa/build_soong.git -b thirteen build/soong
+rm -rf packages/services/Telecomm && git clone https://github.com/Skallfa/packages_services_Telecomm.git -b thirteen-plus packages/services/Telecomm
 
 # Clone Kernel
 rm -rf kernel/xiaomi && git clone https://github.com/PSKMaxi/kernel_xiaomi_sdm845-common.git -b main kernel/xiaomi/sdm845-common --depth=1
