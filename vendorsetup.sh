febf5583297a06e30dbf36d76cc757bf57fc5f9e
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-02-25 13:11:56 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,9 @@ git clone https://github.com/Skallfa/device_xiaomi_sdm845-common.git -b alp devi
 # clone hardware
 rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 
+# clone settings resources
+rm -rf packages/resources/devicesettings && git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings
+
 # Clone Clang
 # git clone https://gitlab.com/jjpprrrr/prelude-clang.git -b master clang-prelude --depth=1 && mv clang-prelude prebuilts/clang/host/linux-x86/
 
